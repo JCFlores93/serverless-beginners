7e050d8d2e08cc42e66f99c80db96a87ecea2178
Jean Carlo Flores <jeancarlo.flores@avantica.net>
2019-02-06 16:29:45 -0500

    adding first function with serverless

---
+++ handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports.hello = async (event, context) => {
+  let message = 'Hello World'
+  const name = event.queryStringParameters && event.queryStringParameters.name
+  console.log(name)
+  if (name) {
+    message = 'Hello ' + name
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: message,
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
